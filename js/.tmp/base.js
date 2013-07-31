045da88dfe52ed737a66eccab15235311dde47fe
Gustavo Felizola <gustavo.felizola@tribointeractive.com.br>
2013-07-31 15:27:30 -0300

    adicionando job e iniciando db.js real (sem textos ainda)

--- js/.tmp/base.js
+++ js/.tmp/base.js
@@ -2,29 +2,26 @@ var siteData = {
     home: {
         slides: [
             {
-                titulo: 'Teste',
-                descricao: 'teste',
-                bg: '#ff0000',
+                titulo: 'Clear Fusão Herbal',
+                descricao: 'Paolla Conta Tudo',
                 imagens: [
-                    'http://lorempixel.com/1000/400/people',
-                    'http://lorempixel.com/1000/400/nature',
-                    'http://lorempixel.com/1000/400/abstract',
+                    'img/jobs/clear/image-1.jpg',
+                    'img/jobs/clear/image-2.jpg',
+                    'img/jobs/clear/image-3.jpg'
+                ]
+            },
+            {
+                titulo: 'Colegio Mackenzie',
+                descricao: 'Site do colegio Mackenzie',
+                imagens: [
+                    'img/jobs/mack/image-1.jpg',
+                    'img/jobs/mack/image-2.jpg',
+                    'img/jobs/mack/image-3.jpg'
                 ]
             },
             {
                 titulo: 'Teste',
                 descricao: 'teste',
-                bg: '#000000',
-                imagens: [
-                    'http://lorempixel.com/1000/400/people',
-                    'http://lorempixel.com/1000/400/nature',
-                    'http://lorempixel.com/1000/400/abstract',
-                ]
-            },
-            {
-                titulo: 'Teste',
-                descricao: 'teste',
-                bg: '#00ff00',
                 imagens: [
                     'http://lorempixel.com/1000/400/people',
                     'http://lorempixel.com/1000/400/nature',
--- js/db.js
+++ js/db.js
@@ -2,21 +2,21 @@ var siteData = {
     home: {
         slides: [
             {
-                titulo: 'Teste',
-                descricao: 'teste',
+                titulo: 'Clear Fusão Herbal',
+                descricao: 'Paolla Conta Tudo',
                 imagens: [
-                    'http://lorempixel.com/1000/400/people',
-                    'http://lorempixel.com/1000/400/nature',
-                    'http://lorempixel.com/1000/400/abstract',
+                    'img/jobs/clear/image-1.jpg',
+                    'img/jobs/clear/image-2.jpg',
+                    'img/jobs/clear/image-3.jpg'
                 ]
             },
             {
-                titulo: 'Teste',
-                descricao: 'teste',
+                titulo: 'Colegio Mackenzie',
+                descricao: 'Site do colegio Mackenzie',
                 imagens: [
-                    'http://lorempixel.com/1000/400/people',
-                    'http://lorempixel.com/1000/400/nature',
-                    'http://lorempixel.com/1000/400/abstract',
+                    'img/jobs/mack/image-1.jpg',
+                    'img/jobs/mack/image-2.jpg',
+                    'img/jobs/mack/image-3.jpg'
                 ]
             },
             {
